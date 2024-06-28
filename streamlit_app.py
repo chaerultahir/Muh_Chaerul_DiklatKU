d3aefba9ce3a5a16fc6c9cd26e3e8e8b9f5f8774
Muh. Chaerul <75104748+chaerultahir@users.noreply.github.com>
2024-06-28 13:30:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st 
-import pandas as pd 
-#import requests
-#from st_aggrid import AgGrid
+import pandas as pd
+import plotly.express as px 
+import matplotlib.pyplot as plt
+import requests
 
+#from st_aggrid import AgGrid
 
 #baca dataframe dari file csv 
 house = pd.read_csv('house_clean.csv')
@@ -10,23 +12,38 @@ house = pd.read_csv('house_clean.csv')
 def main() : 
   st.header('Halaman Streamlit Muh. Chaerul')
   
+  # Menampilkan Metrics
   st.write('Metrics')
   st.metric(label="Temperature", value="25 °C", delta="-1.2 °C")
   
+  # Menampilkan Tabel
   st.subheader('Menampilkan Tabel House')
   st.write('yang bersumber dari tabel house_clean.csv')
   st.markdown('# Data Tabel House')
   st.dataframe(house)
 
 
+  #matplotlib chart 
+  fig,ax = plt.subplots()
+  plt.scatter(house['bedrooms'],house['price'])
+  st.pyplot(fig)
+  plotly_fig = px.scatter(house['bendrooms'],house['price'])
+  st.plotly_chart(plotly_fig)
+
+
+
+
+
+
   
+  # Membuat Button
   click_me_btn = st.button('Click Me')
   st.write(click_me_btn) #Return True kalo di Click 
   check_btn = st.checkbox('Klik Jika Setuju')
   if check_btn :
     st.write('Anda Setuju')
     
-    
+  # Membuat Radio Button
   radio_button= st.radio('Choose below',[x for x in range(1,3)])
   st.write('Anda Memilih',radio_button)
     
