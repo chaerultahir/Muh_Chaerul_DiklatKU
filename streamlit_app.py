080c7b11dc5de76ebed746956804eca6c7cf5d12
Muh. Chaerul <75104748+chaerultahir@users.noreply.github.com>
2024-07-01 01:35:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,6 +62,125 @@ def main() :
     st.metric(label="Selisih", value="30", delta="10 %")
   
 #-------------------------------------------------------------------------------------------------------------
+  #sidebar 
+  sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+  sidebar_radio_button = st.sidebar.radio('Aplikasi Prediksi Harga Rumah',options=['Data House','Deskripsi Data','Prediction'])
+
+   ###Tombol untuk memindahkan konten
+  if st.sidebar.button("Tampilkan di Mainbar"):
+     st.session_state['show_content'] = True
+  else:
+     st.session_state['show_content'] = False
+
+# Pilihan halaman dengan tabs
+  tab = st.sidebar.radio("Aplikasi Jenis bunga Iris:", ["Iris Data", "Visualisasi", "Prediction"])
+
+#-------------------------------------------------------------------------------------------------------------
+  
+  # Menampilkan Tabel
+  st.subheader('Menampilkan Tabel House')
+  st.write('yang bersumber dari tabel house_clean.csv')
+  st.markdown('# Data Tabel House')
+  st.dataframe(house)
+
+  #matplotlib chart 
+  st.subheader('Menampilkan Chart')
+  fig,ax = plt.subplots()
+  plt.scatter(house['bedrooms'],house['price'])
+  st.pyplot(fig)
+  plotly_fig = px.scatter(house['bedrooms'],house['price'])
+  st.plotly_chart(plotly_fig)
+  
+  # Membuat Button
+  st.subheader('Menampilkan Button')
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+    st.write('Anda Setuju')
+    
+  # Membuat Radio Button
+  st.subheader('Menampilkan Radio Button')
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+  st.write('Anda Memilih',radio_button)
+    
+  #slider 
+  st.subheader('Menampilkan Slider')
+  age_slider = st.slider('Berapa Usia Anda',0,100)
+  st.write('Usia Anda',age_slider)
+    
+  #Input (Typing)
+  st.subheader('Menampilkan Input kalkulator Sederhana')
+  num_input = st.number_input('Input Berapapun')
+  st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+  
+
+    
+  #columns :
+  st.subheader('Menampilkan gambar pada 3 kolom')
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+  #atau dengan assignment 
+  image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+  with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+  with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+  
+
+  #expander 
+  #dengan with atau dengan assignment 
+  st.subheader('Menampilkan Expander')
+  expander = st.expander("Klik Untuk Detail ")
+  expander.write('Anda Telah Membuka Detail')
+
+
+
+
+  #Menampilkan hasil di mainbar
+  st.title("Mainbar")
+  if 'show_content' in st.session_state and st.session_state['show_content']:
+     st.write(f"Teks dari sidebar: {input_text}")
+     st.write(f"Angka dari sidebar: {input_number}")
+  else:
+     st.write("Tidak ada konten untuk ditampilkan.")
+
+    #sidebar 
+  st.subheader('Menampilkan sidebar')
+  with st.form("Data Diri"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+  # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+      st.write("slider", slider_val, "checkbox", checkbox_val)
+  st.write("Outside the form")
+
+
+  
+  # Insert containers separated into tabs:
+  tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+  tab1.write("this is tab 1")
+  tab2.write("this is tab 2")
+
+  # You can also use "with" notation:
+  with tab1:
+     st.radio("Select one:", [1, 2])
+
+
+
+
 
 
 if __name__ == '__main__' : 
@@ -78,6 +197,11 @@ def main() :
     iris_data = pd.DataFrame(iris.data, columns=iris.feature_names)
     iris_data['species'] = [iris.target_names[i] for i in iris.target]
 
+
+
+
+
+    
     # Halaman untuk Show Data dengan filter
     if tab == "Iris Data":
         st.header("Iris Dataset")
