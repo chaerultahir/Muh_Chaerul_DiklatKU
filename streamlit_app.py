9937b39fb3b2e15b3f0be8c9d3b384a7c880abc9
Muh. Chaerul <75104748+chaerultahir@users.noreply.github.com>
2024-07-01 01:13:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ def main() :
      st.session_state['show_content'] = False
 
 # Pilihan halaman dengan tabs
-  tab = st.sidebar.radio("Pilih halaman:", ["Iris Data", "Visualisasi", "Prediction"])
+  tab = st.sidebar.radio("Aplikasi Jenis bunga Iris:", ["Iris Data", "Visualisasi", "Prediction"])
 
 #-------------------------------------------------------------------------------------------------------------
   
